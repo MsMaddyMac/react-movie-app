061251c9a3e28e81635be76e92e21b8df8f26789
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-12-06 23:06:07 -0500

    creates boilerplate in MovieList.js

---
+++ src/components/MovieList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const MovieList = props => {
+  return (
+    <>
+      {props.movies.map((movie, index) => (
+
+        <div>
+
+        </div>
+      ))}
+    </>
+  )
+}
+
+export default MovieList
+
